038c1c17b0cd41e9d880dbd7f53d85a60d520564
zachary mixson <48222554+zachary-mixson@users.noreply.github.com>
2022-09-08 17:08:15 -0400

    Update booth.java

---
+++ booth.java
@@ -0,0 +1,12 @@
+package tollCollectionV3;
+
+public class booth {
+	toll t = new toll();
+	
+	double[] boothTotals = new double [20];
+	double[] paymentTotals = new double [5];
+	
+	double total = 0;
+	
+	
+}
